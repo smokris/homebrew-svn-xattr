59f645e1cbe7228ce6dc8380cf95b6b83fc5760b
Steve Mokris <smokris@softpixel.com>
2018-12-09 14:38:59 -0500

    Fix shasum

--- svn-xattr.rb
+++ svn-xattr.rb
@@ -2,7 +2,7 @@ class SvnXattr < Formula
   desc "A command-line utility to facilitate using the Subversion VCS to manage files with extended attributes"
   homepage "https://github.com/smokris/svn-xattr"
   url "https://github.com/smokris/svn-xattr/archive/1.0.tar.gz"
-  sha256 "7f4ef6a215f52e76b0dae6fefb34ee7f83ae44daef6c5d2d55f9f8614fbb28ab"
+  sha256 "6bcfaafabf84f134fa24987dc9c563d37f468f0f92241065120e1cf3a8490eda"
 
   def install
     system "make"
