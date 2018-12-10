1655bf68a9daa310b6b06ad5f443e1cdc646249a
Steve Mokris <smokris@softpixel.com>
2018-12-10 15:37:21 -0500

    Update to 1.1

--- svn-xattr.rb
+++ svn-xattr.rb
@@ -1,8 +1,8 @@
 class SvnXattr < Formula
   desc "A command-line utility to facilitate using the Subversion VCS to manage files with extended attributes"
   homepage "https://github.com/smokris/svn-xattr"
-  url "https://github.com/smokris/svn-xattr/archive/1.0.tar.gz"
-  sha256 "6bcfaafabf84f134fa24987dc9c563d37f468f0f92241065120e1cf3a8490eda"
+  url "https://github.com/smokris/svn-xattr/archive/1.1.tar.gz"
+  sha256 "68831261bd5ccfddca079f1f755d31626ca887e5275436281626e965e52711da"
 
   def install
     bin.install "svn-xattr"
